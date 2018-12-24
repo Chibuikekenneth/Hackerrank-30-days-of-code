07f4132804d6a3bba350ea8acf78e9b490b51c3c
chibuikekenneth <chibuikekenneth003@gmail.com>
2018-12-24 12:27:37 +0100

    hello world

---
+++ 0 - Hello world/solution.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	var message string
+	message, _ = reader.ReadString('\n')
+
+	fmt.Printf("Hello, World.\n%s", message)
+}
