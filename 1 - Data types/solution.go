cb8206876033e3d67cc37c8cc5d4b27599c53a1f
chibuikekenneth <chibuikekenneth003@gmail.com>
2019-01-01 23:36:08 +0100

    day 1

--- 1 - Data types/solution.go
+++ 1 - Data types/solution.go
@@ -15,31 +15,17 @@ func main() {
 	var s string = "HackerRank "
 
 	scanner := bufio.NewScanner(os.Stdin)
-	var input []string
 
-	// Declare second integer, double, and String variables.
-	var intVar uint64
-	var doubleVar float64
-	var stringVar string
+  var i2 uint64
+	var d2 float64
+	var s2 string
 
-	// Read and save an integer, double, and String to your variables.
-	for scanner.Scan() {
-		input = append(input, scanner.Text())
-	}
+	fmt.Scan(&i2)
+	fmt.Scan(&d2)
+	scanner.Scan()
+	s2 = scanner.Text()
 
-	intText := input[0]
-	intVar, _ = strconv.ParseUint(intText, 10, 64)
-	doubleText := input[1]
-	doubleVar, _ = strconv.ParseFloat(doubleText, 64)
-	stringVar = input[2]
-
-	// Print the sum of both integer variables on a new line.
-	fmt.Println(i + intVar)
-
-	// Print the sum of the double variables on a new line.
-	fmt.Printf("%.1f\n", d+doubleVar)
-
-	// Concatenate and print the String variables on a new line
-	// The 's' variable above should be printed first.
-	fmt.Println(s + stringVar)
+	fmt.Println(i + i2)
+	fmt.Printf("%.1f\n", d+d2)
+	fmt.Printf("%s%s", s, s2)
 }
